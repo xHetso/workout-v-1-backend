fafb16a0a125cec623cda024b003e51131e90525
Hetso <datacener23491248@gmail.com>
2024-03-02 21:27:14 +0500

    Implemented a Postgresql database and configured Prisma

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
-import express from 'express'
-
 import 'colors'
+import dotenv from 'dotenv'
+import express from 'express'
+import morgan from 'morgan'
 
 import authRoutes from './app/auth/auth.routes.js'
+import { prisma } from './app/prisma.js'
+
+dotenv.config()
+
+/*
+TODO:
+	[] - Async error handling for method
+	[] - App.use notFound, errorHandler
+	[] - 
+*/
 
 const app = express()
 
 async function main() {
-	/*if (process.env.NODE_ENV === development) app.use(morgan('dev'))*/
+	if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 	app.use(express.json())
 	app.use('/api/auth', authRoutes)
 
-	const PORT = 5000
+	const PORT = process.env.PORT || 5000
 
 	app.listen(
 		PORT,
 		console.log(
-			`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green
-				.bold
+			`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
 		)
 	)
 }
 
 main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async e => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
