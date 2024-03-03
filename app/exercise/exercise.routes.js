5692a1e2733bf765a15b6ec3f91fcf41d26ec618
Hetso <datacener23491248@gmail.com>
2024-03-04 00:21:49 +0500

    Implemented log of training and exercises

--- app/exercise/exercise.routes.js
+++ app/exercise/exercise.routes.js
@@ -9,6 +9,11 @@ import {
 	updateExercise
 } from './exercise.controller.js'
 import { createNewExerciseLog } from './log/exercise-log.controller.js'
+import { getExerciseLog } from './log/get-exercise-log.controller.js'
+import {
+	completeExerciseLog,
+	updateExerciseLogTime
+} from './log/update-exercise-log.controller.js'
 
 const router = express.Router()
 
@@ -19,6 +24,13 @@ router
 	.put(protect, updateExercise)
 	.delete(protect, deleteExercise)
 
-router.route('/log/:exerciseId').post(protect, createNewExerciseLog)
+router
+	.route('/log/:id')
+	.post(protect, createNewExerciseLog)
+	.get(protect, getExerciseLog)
+
+router.route('/log/time/:id').put(protect, updateExerciseLogTime)
+
+router.route('/log/complete/:id').patch(protect, completeExerciseLog)
 
 export default router
--- app/exercise/log/exercise-log.controller.js
+++ app/exercise/log/exercise-log.controller.js
@@ -5,9 +5,8 @@ import { prisma } from '../../prisma.js'
 // @desc    Create new exerciseLog
 // @route   POST /api/exercises/log/:exerciseId
 // @access  Private
-
 export const createNewExerciseLog = asyncHandler(async (req, res) => {
-	const exerciseId = +req.params.exerciseId
+	const exerciseId = +req.params.id
 
 	const exercise = await prisma.exercise.findUnique({
 		where: {
