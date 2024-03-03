5836a43d28887f0d9a2db468d69fc0037dc8f051
Hetso <datacener23491248@gmail.com>
2024-03-03 20:42:15 +0500

    Implemented log of training and exercises and workout

--- app/exercise/exercise.routes.js
+++ app/exercise/exercise.routes.js
@@ -8,6 +8,7 @@ import {
 	getExercises,
 	updateExercise
 } from './exercise.controller.js'
+import { createNewExerciseLog } from './log/exercise-log.controller.js'
 
 const router = express.Router()
 
@@ -17,5 +18,7 @@ router
 	.route('/:id')
 	.put(protect, updateExercise)
 	.delete(protect, deleteExercise)
+
+router.route('/log/:exerciseId').post(protect, createNewExerciseLog)
 
 export default router
