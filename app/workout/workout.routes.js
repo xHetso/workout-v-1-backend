0b3d3541fe932a0c062ef836aab871b5c72eead3
Hetso <datacener23491248@gmail.com>
2024-03-05 00:31:08 +0500

    Add workout log controllers and routes

--- app/workout/workout.routes.js
+++ app/workout/workout.routes.js
@@ -2,6 +2,9 @@ import express from 'express'
 
 import { protect } from '../middleware/auth.middleware.js'
 
+import { getWorkoutLog } from './log/get-workout-log.controller.js'
+import { updateCompleteWorkoutLog } from './log/update-workout-log.controller.js'
+import { createNewWorkoutLog } from './log/workout-log.controller.js'
 import {
 	createNewWorkout,
 	deleteWorkout,
@@ -19,5 +22,12 @@ router
 	.get(protect, getWorkout)
 	.put(protect, updateWorkout)
 	.delete(protect, deleteWorkout)
+
+router
+	.route('/log/:id')
+	.post(protect, createNewWorkoutLog)
+	.get(protect, getWorkoutLog)
+
+router.route('/log/complete/:id').patch(protect, updateCompleteWorkoutLog)
 
 export default router
